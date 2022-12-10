ce80e68d93c52bb6a11b79615c5ca3ff80f6b3a5
Sunanda Wasala <sunanda@listudiosl.com>
2022-12-10 18:23:10 +0530

    login and middware /JWt

--- Server/routes/hotels.js
+++ Server/routes/hotels.js
@@ -1,34 +1,26 @@
 import express from 'express';
-import Hotel from '../models/Hotel.js';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
+
 
 const router=express.Router();
 
 
 //CREATE
-router.post("/", async (req,res)=>{
-
-
-const newHotels=new Hotel(req.body);
-
-    try {
-
-        const saveHotel= await newHotels.save()
-
-        res.status(200).json(saveHotel)
-        
-    } catch (err) {
-
-        res.status(500).json(err)
-        
-    }
-
-})
-
+router.post("/",verifyAdmin, createHotel);
 
 //UPDATE
+router.put("/:id",verifyAdmin, updateHotel);
+
 //DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+
 //GET
+router.get("/:id", getHotel);
+
+
 //GET ALL
+router.get("/", getHotels);
 
 
 export default router
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.js';
+import { verifyToken, verifyUser,verifyAdmin } from '../utils/verifyToken.js';
 
 const router=express.Router();
+
+
+//UPDATE
+router.put("/:id",verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id",verifyUser, deleteUser);
+
+//GET
+router.get("/:id",verifyUser, getUser);
+
+
+//GET ALL
+router.get("/",verifyAdmin, getUsers);
+
+
 
 
 
